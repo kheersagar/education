0175a268899e620ac864b737134a9c19d4dbaa63
kheersagar parja <73149618+kheersagar@users.noreply.github.com>
2023-03-04 13:47:42 +0530

    Updated

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const database = require('./mongoDB_connection');
 const userRouter = require("./routes/user")
 const PORT = process.env.PORT || 5000;
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
