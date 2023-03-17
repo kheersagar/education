be5e0db781c0b0c5c80d071ad05870b276b51767
kheersagar <santparja@gmail.com>
2023-03-17 15:08:04 +0530

    changes submit test route

--- controller/testController.js
+++ controller/testController.js
@@ -20,15 +20,15 @@ const submitTest = async (req, res) => {
     // console.log(req.body)
     let marks_obtained = 0, testResult= [],correct_answers = 0 ,wrong_ansuwers=0;
     let recommend = questions.map(async (item, index) => {
-      const answers = await question.findOne({_id:item.id})
+      const data = await question.findOne({_id:item.id})
       // calculate user's performance
       testResult.push({
-        ...answers._doc,
+        ...data._doc,
         userAnswer: item.answer,
-        status : answers.answer == item.answer ? "Correct" : "Wrong"
+        status : data.answer == item.answer ? "Correct" : "Wrong"
       })
       // calculate total marks
-      if(answers.answer == item.answer){
+      if(data.answer == item.answer){
         marks_obtained +=1
         correct_answers +=1
       }else{
@@ -36,7 +36,7 @@ const submitTest = async (req, res) => {
       }
       // get recommendation from the model
       const result = await axios.post("https://repeatation.onrender.com/recommend", {
-        question: item.data,
+        question: data.question,
         n: 1,
       });
       return result.data
